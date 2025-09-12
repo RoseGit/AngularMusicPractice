50646f56c698629275967ea680b5fa185052bfd0
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2025-09-12 19:53:02 +0000

    login form

--- curso-angular-cli/src/app/app.module.ts
+++ curso-angular-cli/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-
+import { UserEditComponent } from './components/user-edit.component';
+import { routing, appRoutingProviders } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing, 
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
